2b6d41cddf7a55c0aed2ae4449df01ea7cbd0aee
Anshul Hedau <anshulhedau01@gmail.com>
2019-03-16 11:25:21 +0530

    Major Modifications (Working with dataset) :8ball:

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 from flask import Flask, jsonify
 from sklearn.externals import joblib
 import pandas as pd
-import numpy as np
+import numpy as np,scipy.stats
 # Flask object creation
 app = Flask(__name__)
 
@@ -27,23 +27,35 @@ def index():
 def predict():
      json_ = request.json
      myvar = request.args["myvar"]
-     print(myvar[0])
-     temp = myvar.split(',')
+     parameter_list = myvar.split(',')
+     myvar = parameter_list[0]
+     upper_value = int(parameter_list[1])
+     lower_value = int(parameter_list[2])
+     print(myvar)
+     len_array = len(myvar)
+     list_filtered_values = []
+     for i in range(0,len_array):
+         if (myvar[i] == '0'):
+             list_filtered_values.append(lower_value)
+         else:
+             list_filtered_values.append(upper_value)
+     #temp = myvar.split(',')
+     print(list_filtered_values)
      inputarray = []
-     for item in temp:
-         inputarray.append(float(item))
+     inputarray.append([np.amin(list_filtered_values),np.amax(list_filtered_values),np.ptp(list_filtered_values),
+                           np.percentile(list_filtered_values,75),np.percentile(list_filtered_values,25),
+                           np.median(list_filtered_values),np.mean(list_filtered_values),np.std(list_filtered_values),
+                           np.var(list_filtered_values),scipy.stats.kurtosis(list_filtered_values),
+                           scipy.stats.skew(list_filtered_values)])
      print(inputarray)
      #query_df = pd.DataFrame(json_)
      #query = pd.get_dummies(query_df)
-     #inputarray = np.array([[411.0,417.0,6.0,417.0,411.0,417.0,414.54,2.95,8.71,-1.87,-0.37]])
-     #input_query - np.array([[411.0,417.0,6.0,417.0,411.0,417.0,414.54,2.95,8.71,-1.87,-0.37]])
-     clf = joblib.load('model.pkl')
-     prediction = clf.predict([inputarray])
-     percentage_predictions = clf.predict_proba([inputarray])
+     prediction = clf.predict(inputarray)
+     percentage_predictions = clf.predict_proba(inputarray)
      #print(type(percentage_predictions))
      percentage_predictions = np.array(percentage_predictions.tolist())
      print(percentage_predictions[0][0])
-     return jsonify({'prediction': list(prediction),'ceremics':percentage_predictions[0][0],'paper':percentage_predictions[0][1]})
+     return jsonify({'prediction': list(prediction),'ceremics':percentage_predictions[0][0],'paper':percentage_predictions[0][1],'wood':0,'metal':0})
 
 
 # Help page
