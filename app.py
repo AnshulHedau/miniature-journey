ea9bffd238f7b4870a7864fdbd203e70979ac800
Anshul Hedau <anshulhedau01@gmail.com>
2019-03-28 17:01:36 +0530

    Test Commit

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ import json
 import requests
 import time
 import re
+import pyrebase
+import keras
+import tensorflow as tf
 from datetime import datetime, timedelta
 from bs4 import BeautifulSoup
 from flask import Flask, jsonify
 from sklearn.externals import joblib
 import pandas as pd
 import numpy as np,scipy.stats
+
+from keras.models import load_model
 # Flask object creation
 app = Flask(__name__)
 
@@ -18,44 +23,47 @@ app = Flask(__name__)
 # Index page
 @app.route("/")
 def index():
-    return_value = {"message": "Welcome to the Notify-IPL API!"}
+    return_value = {"message": "Welcome to the Miniature Journey API!"}
     json_string = json.dumps(return_value)
     return json_string
 
 '''@app.route('/predict', methods=['POST'])'''
-@app.route('/predict', methods=['GET'])
+@app.route('/predict')
 def predict():
      json_ = request.json
-     clf = joblib.load('model.pkl')
-     myvar = request.args["myvar"]
-     parameter_list = myvar.split(',')
-     myvar = parameter_list[0]
-     upper_value = int(parameter_list[1])
-     lower_value = int(parameter_list[2])
-     print(myvar)
-     len_array = len(myvar)
-     list_filtered_values = []
-     for i in range(0,len_array):
-         if (myvar[i] == '0'):
-             list_filtered_values.append(lower_value)
+     model = load_model("my_model.h5")
+     config = {
+         "apiKey": "AIzaSyDNthbeIvXGp5AkpiHA2yMmH5SJ8ww75CQ",
+         "authDomain": "lien-1553715274263.firebaseapp.com",
+         "databaseURL": "https://lien-1553715274263.firebaseio.com",
+         "storageBucket": "lien-1553715274263.appspot.com"
+     }
+
+     firebase = pyrebase.initialize_app(config)
+     fb_database = firebase.database()
+
+     stored_data = fb_database.child('car_data').get().val()
+     z_gyro = []
+     mean_gyro = [-0.10162200279091103]
+     for key, value in stored_data.items():
+         if (len(value) > 4):
+             z_gyro.append(value['gyro_z'])
+             mean_gyro = [value['gyro_z']]
          else:
-             list_filtered_values.append(upper_value)
-     #temp = myvar.split(',')
-     print(list_filtered_values)
-     inputarray = []
-     inputarray.append([np.amin(list_filtered_values),np.amax(list_filtered_values),np.ptp(list_filtered_values),
-                           np.percentile(list_filtered_values,75),np.percentile(list_filtered_values,25),
-                           np.median(list_filtered_values),np.mean(list_filtered_values),np.std(list_filtered_values),
-                           np.var(list_filtered_values),scipy.stats.kurtosis(list_filtered_values),
-                           scipy.stats.skew(list_filtered_values)])
-     print(inputarray)
-     #query_df = pd.DataFrame(json_)
-     #query = pd.get_dummies(query_df)
-     prediction = clf.predict(inputarray)
-     percentage_predictions = clf.predict_proba(inputarray)
-     #print(type(percentage_predictions))
-     percentage_predictions = np.array(percentage_predictions.tolist())
-     print(percentage_predictions[0][0])
+             z_gyro.append(mean_gyro[0])
+
+     z_gyro = z_gyro[-25:]
+     test_gyro = []
+     test_gyro.append(z_gyro)
+     test_gyro = np.array(test_gyro)
+     print(len(test_gyro[0]))
+     result_test = model.predict([test_gyro])
+
+     if (result_test[0][1] > result_test[0][0]):
+         print("Rash")
+     else:
+         print("Normal")
+
      return jsonify({'prediction': list(prediction),'ceremics':percentage_predictions[0][0],'paper':percentage_predictions[0][1],'wood':0,'metal':0})
 
 
